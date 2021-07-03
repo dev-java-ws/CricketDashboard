72eb32240e59b8796705bf5dac0d54f7230505fd
devia <devia@host.docker.internal>
2021-07-03 00:17:00 -0500

    Adding React final UI Code changes

---
+++ cricket-dashboard/src/frontendreact/src/pages/MatchPage.js
@@ -0,0 +1,12 @@
+import { React } from 'react';
+
+export const MatchPage = () => {
+	
+	return (
+    <div className="MatchPage"> 
+    	<h1> MatchPage </h1>
+    </div>
+  );
+}
+ 
+
--- cricket-dashboard/src/frontendreact/src/pages/TeamPage.js
+++ cricket-dashboard/src/frontendreact/src/pages/TeamPage.js
@@ -1,15 +1,17 @@
 import { React, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../component/MatchDetailCard';
 import { MatchSmallCard } from '../component/MatchSmallCard';
 
 export const TeamPage = () => {
 	
 	const [team, setTeam] = useState({matches: []});
+	const { teamName } = useParams();
 	
 	useEffect(
 		() => {
 			const fetchMatches = async () => {
-				const response = await fetch('http://localhost:8090/team/Chennai%20Super%20Kings');
+				const response = await fetch(`http://localhost:8090/team/${teamName}`);
 				const data = await response.json();
 				console.log(data);
 				setTeam(data);
@@ -17,17 +19,19 @@ export const TeamPage = () => {
 			};
 			fetchMatches();
 			
-		}, []	
+		}, [teamName]	
 	
 	);
 	
-  return (
+	if(!team || !team.teamName) {return <h1> Team Not Found </h1>}
+ 
+	return (
     <div className="TeamPage"> 
     	<h1> {team.teamName} </h1>
     	
-    	 <MatchDetailCard match={team.matches[0]}/>
+    	 <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
     	 
-    	 {team.matches.slice(1).map(match => <MatchSmallCard match={match} />)}
+    	 {team.matches.slice(1).map(match => <MatchSmallCard  teamName={team.teamName}  match={match} />)}
 
     </div>
   );
@@ -36,3 +40,5 @@ export const TeamPage = () => {
 //creating a state using useState, team is a state and using setTeam to populate state
 // 	 call to useEffect   	 when component loads which is [] array, call fetchMatches, calls url and sets response
 //use {team.teamName} JSX to render values
+    	 
+    	 //to send to backend use react router http://localhost:3000/teams/Chennai%20Super%20Kings 
